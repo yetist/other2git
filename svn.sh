28f8178e60e2268e96f5cdd7329e345b818fa654
Wu Xiaotian <yetist@gmail.com>
2019-06-20 23:44:34 +0800

    update

---
+++ svn.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#svn co https://crosswire.org/svn/sword/trunk/ sword
+#cd sword
+#svn log -q | awk -F '|' '/^r/ {sub("^ ", "", $2); sub(" $", "", $2); print $2" = "$2" <"$2">"}' | sort -u > authors-transform.txt
+svnurl=https://crosswire.org/svn/sword/
+
+svn log -q $svnurl | awk -F '|' '/^r/ {sub("^ ", "", $2); sub(" $", "", $2); print $2" = "$2" <"$2">"}' | sort -u > authors-transform.txt
+git svn clone $svnurl --no-metadata -A authors-transform.txt --stdlayout ~/temp
+
+cd ~/temp
+git svn show-ignore > .gitignore
+git add .gitignore
+git commit -m 'Convert svn:ignore properties to .gitignore.'
+
+git init --bare ~/new-bare.git
+cd ~/new-bare.git
+git symbolic-ref HEAD refs/heads/trunk
+
+
+cd ~/temp
+git remote add bare ~/new-bare.git
+git config remote.bare.push 'refs/remotes/*:refs/heads/*'
+git push bare
+
+cd ~/new-bare.git
+git branch -m trunk master
+
+
+cd ~/new-bare.git
+git for-each-ref --format='%(refname)' refs/heads/tags |
+cut -d / -f 4 |
+while read ref
+do
+  git tag "$ref" "refs/heads/tags/$ref";
+  git branch -D "tags/$ref";
+done
